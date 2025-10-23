0bda8efa071fa2ce559192dd86daa7e2ea3915ee
Lucia Nigai <luchianigaj@gmail.com>
2025-10-23 22:44:14 +0300

    memoria training service

---
+++ src/test/java/com/lucia/memoria_training/MemoriaTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucia.memoria_training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoriaTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

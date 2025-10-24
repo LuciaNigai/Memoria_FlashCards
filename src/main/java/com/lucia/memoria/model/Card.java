0aedd0045c5bd5e7ef290d6d9f15f756a9ea67fe
Lucia Nigai <luchianigaj@gmail.com>
2025-10-24 19:40:49 +0300

    added indexes and createAt updatedAt for card

--- src/main/java/com/lucia/memoria/model/Card.java
+++ src/main/java/com/lucia/memoria/model/Card.java
@@ -1,18 +1,8 @@
 package com.lucia.memoria.model;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -20,6 +10,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.UUID;
 
@@ -29,6 +22,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @Entity
 @Table(name = "cards")
+@EntityListeners(AuditingEntityListener.class)
 public class Card {
 
   @Id
@@ -56,6 +50,14 @@ public class Card {
       inverseJoinColumns = @JoinColumn(name = "tag_id")
   )
   private Set<Tag> tags;
+
+  @CreatedDate
+  @Column(name = "created_at", nullable = false, updatable = false)
+  private Instant createdAt;
+
+  @LastModifiedDate
+  @Column(name = "updated_at", nullable = false)
+  private Instant updatedAt;
 
   public void addField(Field field) {
     fields.add(field);

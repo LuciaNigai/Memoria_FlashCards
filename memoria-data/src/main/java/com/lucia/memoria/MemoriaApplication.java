bb26841f59a4a44afffd6bdc00e9c0b4c6148006
Lucia Nigai <luchianigaj@gmail.com>
2025-10-24 19:40:49 +0300

    added indexes and createAt updatedAt for card

--- memoria-data/src/main/java/com/lucia/memoria/MemoriaApplication.java
+++ memoria-data/src/main/java/com/lucia/memoria/MemoriaApplication.java
@@ -2,8 +2,10 @@ package com.lucia.memoria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MemoriaApplication {
 
     public static void main(String[] args) {
